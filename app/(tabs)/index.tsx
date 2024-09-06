efc363d436643366c01caf532a65c24f0f24db36
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-06 23:51:30 +0200

    use <link> from expo to navigate to the /profile

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
+import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View style={styles.titleContainer}>
       <Text>VidAura</Text>
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go to Profile
+      </Link>
     </View>
   );
 }
