f91e1d12c6529170c948cd38c57d23065e14fa32
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-07 20:50:23 +0200

    resize the Logo Image

--- app/index.tsx
+++ app/index.tsx
@@ -8,7 +8,11 @@ export default function App() {
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
         <View className="w-full justify-center items-center h-full px-4">
-          <Image source={images.logo} />
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
