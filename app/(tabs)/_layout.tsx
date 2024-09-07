10383bb5a47bdb54f93e41af984cea1d9704973d
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-07 17:03:31 +0200

    style the home icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,7 +3,18 @@ import React from "react";
 import { Image, View } from "react-native";
 import { icons } from "../../constants";
 
-
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View>
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+    </View>
+  );
+};
 
 export default function TabLayout() {
   return (
@@ -18,7 +29,12 @@ export default function TabLayout() {
           title: "Home",
           headerShown: false,
           tabBarIcon: ({ color, focused }) => (
-           
+            <TabIcon
+              icon={icons.home}
+              color={color}
+              name="Home"
+              focused={focused}
+            />
           ),
         }}
       />
