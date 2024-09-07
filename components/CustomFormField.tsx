dcaad4a40b01e6fc8886de7e442956dcab29dc93
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-07 21:49:09 +0200

    add a Text to show the title, add a TextInput t o accept the user's input

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -1,8 +1,15 @@
-import { StyleSheet, Text, TextInputProps, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TextInputProps,
+  View,
+} from "react-native";
 import React from "react";
 interface CustomFormFieldProps {
   title: string;
   value: string;
+  placeholder: string;
   handleChange: (text: string) => void;
   otherStyles?: string;
   keyboardType?: TextInputProps["keyboardType"];
@@ -10,15 +17,21 @@ interface CustomFormFieldProps {
 const CustomFormField: React.FC<CustomFormFieldProps> = ({
   title,
   value,
+  placeholder,
   handleChange,
   otherStyles,
   keyboardType = "default",
 }) => {
   return (
-    <View className="space-y-2">
-      <Text className="text-base text-gray-100 font-pmedium">
-        CustomFormField
-      </Text>
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+      <View className="w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary flex flex-row items-center">
+        <TextInput
+          className="flex-1 text-white font-psemibold text-base"
+          value={value}
+          placeholder={placeholder}
+        ></TextInput>
+      </View>
     </View>
   );
 };
