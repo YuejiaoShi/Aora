801c6dd6dcdc418a33b3b8d8a4fb422a50b3f4c9
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-08 00:25:55 +0200

    Initialize SDK with Appwrite server API endpoint and project ID

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,11 +1,19 @@
-import { Platform } from "react-native";
+import { Client } from "react-native-appwrite";
 
 export const appwriteConfig = {
   endpoint: "https://cloud.appwrite.io/v1",
-  Platform: "com.yuejiao.aora",
+  platform: "com.yuejiao.aora",
   projectId: "66dcceff0031dfdf6527",
   databaseId: "66dcd0010027a099ba13",
   userCollectionId: "66dcd06d002de94db30e",
   videoCollectionId: "66dcd085001df29217df",
   storageId: "66dcd1d30012490d2657",
 };
+
+// Init your React Native SDK
+const client = new Client();
+
+client
+  .setEndpoint(appwriteConfig.endpoint)
+  .setProject(appwriteConfig.projectId)
+  .setPlatform(appwriteConfig.platform);
