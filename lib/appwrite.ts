e050a750e58a39539398c30ccea25ce9fc6360f3
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-08 00:22:28 +0200

    connect to appwrite project

---
+++ lib/appwrite.ts
@@ -0,0 +1,11 @@
+import { Platform } from "react-native";
+
+export const appwriteConfig = {
+  endpoint: "https://cloud.appwrite.io/v1",
+  Platform: "com.yuejiao.aora",
+  projectId: "66dcceff0031dfdf6527",
+  databaseId: "66dcd0010027a099ba13",
+  userCollectionId: "66dcd06d002de94db30e",
+  videoCollectionId: "66dcd085001df29217df",
+  storageId: "66dcd1d30012490d2657",
+};
