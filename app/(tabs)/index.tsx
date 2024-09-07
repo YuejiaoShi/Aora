216e8735b5b12d7f469dfc3859edbf8370ef71d0
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-07 13:54:55 +0200

    set up nativewind, tailwind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,12 @@
 import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
-
+import { Text, View } from "react-native";
 export default function HomeScreen() {
   return (
-    <View style={styles.titleContainer}>
-      <Text>VidAura</Text>
+    <View className="bg-red-300">
+      <Text>VidAuraffff</Text>
       <Link href="/profile" style={{ color: "blue" }}>
         Go to Profile
       </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flex: 1,
-    backgroundColor: "white",
-    justifyContent: "center",
-    alignContent: "center",
-  },
-});
