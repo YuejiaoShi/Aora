cc324c522285ff8855d8f1cd1c396660a44114ea
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-07 14:48:43 +0200

    add fonts assets, constants, initialize other files

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,17 +1,10 @@
 import { Tabs } from "expo-router";
 import React from "react";
 
-import { TabBarIcon } from "@/components/navigation/TabBarIcon";
-import { Colors } from "@/constants/Colors";
-import { useColorScheme } from "@/hooks/useColorScheme";
-
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
       }}
     >
@@ -19,24 +12,12 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "Home",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "home" : "home-outline"}
-              color={color}
-            />
-          ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: "Profile",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "code-slash" : "code-slash-outline"}
-              color={color}
-            />
-          ),
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,9 +2,9 @@ import { Link } from "expo-router";
 import { Text, View } from "react-native";
 export default function HomeScreen() {
   return (
-    <View className="bg-red-300">
+    <View className="flex items-center justify-center bg-red-200">
       <Text>VidAuraffff</Text>
-      <Link href="/profile" style={{ color: "blue" }}>
+      <Link href="/profile" className="text-xl">
         Go to Profile
       </Link>
     </View>
