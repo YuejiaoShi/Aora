da4cc22e58564c2ea9cf223f5f74670e1eda1ec4
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-07 15:08:50 +0200

    add fonts

--- app/index.tsx
+++ app/index.tsx
@@ -2,9 +2,9 @@ import { Link } from "expo-router";
 import { Text, View } from "react-native";
 export default function HomeScreen() {
   return (
-    <View className="flex items-center justify-center bg-red-200">
+    <View className="flex items-center justify-center bg-yellow-500">
       <Text>VidAuraffff</Text>
-      <Link href="/profile" className="text-xl">
+      <Link href="/profile" className="text-yellow-800 text-xl">
         Go to Profile
       </Link>
     </View>
