7b369918db9a491901f7665caa5343324e432186
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-10 20:13:10 +0200

    add ListHeaderComponent to create the header for each list item

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -10,11 +10,27 @@ const Home = () => {
   ];
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-primary h-full">
       <FlatList
         data={dummyData}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
+        renderItem={({ item }) => (
+          <Text className="text-white">{item.title}</Text>
+        )}
+        ListHeaderComponent={
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome to Aora
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  Yuejiao
+                </Text>
+              </View>
+            </View>
+          </View>
+        }
       />
     </SafeAreaView>
   );
