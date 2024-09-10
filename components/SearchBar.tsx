fdc8526560b022ae2c556c96ec564b6bab9b4d0b
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-10 20:38:35 +0200

    restructure the SearchBar content

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,3 +1,4 @@
+import { icons } from "@/constants";
 import React from "react";
 import {
   Image,
@@ -26,17 +27,17 @@ const SearchBar: React.FC<SearchBarProps> = ({
   keyboardType = "default",
 }) => {
   return (
-    <View className={`space-y-2`}>
-      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
-      <View className="w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary flex flex-row items-center">
-        <TextInput
-          className="flex-1 text-white font-psemibold text-base"
-          value={value}
-          placeholder={placeholder}
-          placeholderTextColor="#7B7B8B"
-          onChangeText={handleChange}
-        />
-      </View>
+    <View className="w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary flex flex-row items-center space-x-4">
+      <TextInput
+        className="mt-0.5 text-base text-white flex-1 font-pregular"
+        value={value}
+        placeholder={placeholder}
+        placeholderTextColor="#7B7B8B"
+        onChangeText={handleChange}
+      />
+      <TouchableOpacity>
+        <Image source={icons.search} className="w-6 h-6" />
+      </TouchableOpacity>
     </View>
   );
 };
