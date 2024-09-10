71ebbbaa75a64c186509be8f32642fd3548bc210
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-10 14:04:13 +0200

    configure submit func

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -4,13 +4,33 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "@/constants";
 import FormField from "@/components/FormField";
 import Button from "@/components/Button";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
+import { Alert } from "react-native";
+import { signIn } from "@/lib/appwrite";
 
 const SignIn = () => {
   const [form, setForm] = useState({ email: "", password: "" });
-  const [isSubmiting, setIsSubmiting] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const handleSubmit = () => {};
+  const handleSubmit = async () => {
+    if (!form.email || !form.password) {
+      Alert.alert("Error", "Please fill in all fields");
+      return;
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      const signInResult = await signIn(form.email, form.password);
+
+      router.replace("/home");
+    } catch (error) {
+      const errorMessage = (error as Error).message || "Unknown error occurred";
+      Alert.alert("Error", errorMessage);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -45,7 +65,7 @@ const SignIn = () => {
             title="Sign In"
             handlePress={handleSubmit}
             containerStyles="mt-6"
-            isLoading={isSubmiting}
+            isLoading={isSubmitting}
           ></Button>
           <View className="justify-center pt-6 gap-2 flex-row">
             <Text className="text-lg text-gray-100 font-pregular">
