ed601ab85f1e9a5580e9a5d05cb800c53cd7e0c2
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-10 22:25:31 +0200

    add a button to redirect user to the create page

--- components/EmptyState.tsx
+++ components/EmptyState.tsx
@@ -1,6 +1,8 @@
 import { View, Text, Image } from "react-native";
 import React from "react";
 import { images } from "@/constants";
+import Button from "./Button";
+import { router } from "expo-router";
 
 interface EmptyStateProps {
   title: string;
@@ -15,6 +17,12 @@ const EmptyState: React.FC<EmptyStateProps> = ({ title, subtitle }) => {
       <Text className="font-pmedium mt-2 text-sm text-gray-100">
         {subtitle}
       </Text>
+
+      <Button
+        title="Create a video"
+        handlePress={() => router.push("/create")}
+        containerStyles="my-5 w-full"
+      />
     </View>
   );
 };
