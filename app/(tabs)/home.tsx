c4564f04fd6653308763d329b0f61e6854fdfe7f
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-10 20:04:59 +0200

    use SafeAreaView and FlatList to list an array of data

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,11 +1,22 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
+  const dummyData = [
+    { id: "1", title: "Item 1" },
+    { id: "2", title: "Item 2" },
+    { id: "3", title: "Item 3" },
+  ];
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+      <FlatList
+        data={dummyData}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Text>{item.title}</Text>}
+      />
+    </SafeAreaView>
   );
 };
 
