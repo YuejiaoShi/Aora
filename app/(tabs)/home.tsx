bad93ea9371d82a75ab96a264f5be93b37225b9b
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-10 20:43:39 +0200

    add placeholder prop to SearchBar

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -39,7 +39,7 @@ const Home = () => {
               </View>
             </View>
 
-            <SearchBar />
+            <SearchBar placeholder="Search for a video topic..." />
             <View>
               <Text className="text-sm text-gray-100 text-lg font-pregular">
                 Latest Videos
