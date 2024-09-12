73dbb2603ac589d16034debe69f0dcfce349544a
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-12 22:20:50 +0200

    add a play state to handle show something, and a button to trigger the setPlay

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import * as Animatable from "react-native-animatable";
-import { FlatList, Text } from "react-native";
+import { FlatList, Text, TouchableOpacity } from "react-native";
 
 interface TrendingProps {
   posts: any;
@@ -11,17 +11,24 @@ const zoomIn = { 0: { scale: 0.9 }, 1: { scale: 1 } };
 const zoomOut = { 0: { scale: 1 }, 1: { scale: 0.9 } };
 
 const TrendingItem: React.FC<any> = ({ activeItem, item }) => {
+  const [play, setPlay] = useState(false);
   return (
     <Animatable.View
       animation={activeItem === item.$id ? "zoomIn" : "zoomOut"}
       duration={500}
       className="mr-5 text-white"
-    ></Animatable.View>
+    >
+      {play ? (
+        <Text className="text-2xl font-pmedium text-white">playing</Text>
+      ) : (
+        <TouchableOpacity onPress={() => setPlay(true)}></TouchableOpacity>
+      )}
+    </Animatable.View>
   );
 };
 
 const Trending: React.FC<TrendingProps> = ({ posts }) => {
-  const [activeItem, setActiveItem] = React.useState(posts[0]);
+  const [activeItem, setActiveItem] = useState(posts[0]);
 
   return (
     <FlatList
