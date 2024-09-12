795924da345dd64c2dbeffaab9cc13f8ea3227e7
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-12 22:08:51 +0200

    add a TrendingItem to use Animatable imported from react-native

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -1,17 +1,30 @@
 import React from "react";
+import * as Animatable from "react-native-animatable";
 import { FlatList, Text } from "react-native";
 
 interface TrendingProps {
   posts: any;
 }
 
+const TrendingItem: React.FC<any> = ({ activeItem, item }) => {
+  return (
+    <Animatable.View
+      animation="fadeIn"
+      duration={500}
+      className="mr-5 text-white"
+    ></Animatable.View>
+  );
+};
+
 const Trending: React.FC<TrendingProps> = ({ posts }) => {
+  const [activeItem, setActiveItem] = React.useState(posts[0]);
+
   return (
     <FlatList
       data={posts}
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => (
-        <Text className="text-white">{item.title}</Text>
+        <TrendingItem activeItem={activeItem} item={item} />
       )}
       horizontal
     />
