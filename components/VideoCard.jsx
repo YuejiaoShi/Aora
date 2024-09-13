d2bc4de59154d7a85861ffaef653b81b044cdc0d
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-13 22:20:14 +0200

    rewrite tsx into jsx

---
+++ components/VideoCard.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, Image, Alert } from "react-native";
+import { Video } from "expo-av";
+import WebView from "react-native-webview";
+import { icons } from "../constants";
+
+const VideoCard = ({ title, creator, avatar, thumbnail, video }) => {
+  const [play, setPlay] = useState(false);
+
+  const isEmbedded =
+    video.includes("youtube.com") || video.includes("vimeo.com");
+
+  return (
+    <View className="flex flex-col items-center px-4 mb-14">
+      <View className="flex flex-row gap-3 items-start">
+        <View className="flex flex-row items-center flex-1">
+          <View className="w-[46px] h-[46px] rounded-lg border border-secondary flex justify-center items-center p-0.5">
+            <Image
+              source={{ uri: avatar }}
+              className="w-full h-full rounded-lg"
+              resizeMode="cover"
+            />
+          </View>
+          <View className="flex flex-1 ml-3">
+            <Text
+              className="font-semibold text-sm text-white"
+              numberOfLines={1}
+            >
+              {title}
+            </Text>
+            <Text className="text-xs text-gray-100" numberOfLines={1}>
+              {creator}
+            </Text>
+          </View>
+        </View>
+      </View>
+
+      {play ? (
+        isEmbedded ? (
+          <View className="w-full h-60 rounded-xl mt-3 relative">
+            <WebView
+              source={{ uri: video }}
+              className="w-full h-full"
+              allowsInlineMediaPlayback
+              javaScriptEnabled
+              allowsFullscreenVideo
+            />
+          </View>
+        ) : (
+          <View className="w-full h-60 rounded-xl mt-3 relative">
+            <Video
+              source={{ uri: video }}
+              className="w-full h-full"
+              useNativeControls
+              resizeMode="cover"
+              shouldPlay
+              onPlaybackStatusUpdate={(status) => {
+                if (status.didJustFinish) {
+                  setPlay(false);
+                }
+              }}
+              onError={(error) => {
+                console.error("Failed to load video:", error);
+                setPlay(false);
+              }}
+            />
+          </View>
+        )
+      ) : (
+        <TouchableOpacity
+          className="w-full h-60 rounded-xl mt-3 relative flex justify-center items-center"
+          activeOpacity={0.7}
+          onPress={() => setPlay(true)}
+        >
+          <Image
+            source={{ uri: thumbnail }}
+            className="w-full h-full rounded-xl"
+            resizeMode="cover"
+          />
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+};
+
+export default VideoCard;
