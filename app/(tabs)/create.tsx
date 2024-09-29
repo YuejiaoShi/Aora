26fd6530a1c37261afe7ac53f7fd87ec54cf52aa
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-29 23:50:07 +0200

    create a FormField to ask for video title

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,12 +1,29 @@
 import { ScrollView, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import FormField from "@/components/FormField";
 
 const Create = () => {
+  const [upLoading, setUpLoading] = useState(false);
+  const [form, setForm] = useState({
+    title: "",
+    video: null,
+    thumbnail: null,
+    prompt: "",
+  });
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView className="px-4 my-6">
         <Text className="text-2xl text-white font-psemibold">Upload Video</Text>
+
+        <FormField
+          title="Video Title"
+          placeholder="Enter your video title"
+          value={form.title}
+          handleChange={(e) => setForm({ ...form, title: e })}
+          otherStyles="mt-4"
+        />
       </ScrollView>
     </SafeAreaView>
   );
